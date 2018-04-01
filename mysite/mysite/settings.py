438ed34319552165664f0ef964f4217c5060f535
深澤風土 <kazeto@kazeto.local>
2018-04-02 00:06:40 +0900

    add blog of bootstrap

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import KEY
+from . import KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -121,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '.static'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
